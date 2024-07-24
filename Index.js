71a129c00bbb38ad12942a4a54b3e46c7781448d
MinorSc <misolano1192@gmail.com>
2024-07-23 20:11:17 -0600

    Feat Tarea

--- Index.js
+++ Index.js
@@ -1,3 +1,7 @@
+
+console.log("hola")
+
+
 let series = [
     { id: 1, Serie: "Ana", Categoria: "Drama", Temporadas: 2 },
     { id: 2, Serie: "Stranger Things", Categoria: "Ciencia Ficción", Temporadas: 4 },
@@ -5,12 +9,53 @@ let series = [
     { id: 4, Serie: "La Casa de Papel", Categoria: "Crimen", Temporadas: 5 }
 ];
 
+//1
+mostrarNombresSeries(series)
+
 function mostrarNombresSeries(datos) {
-    // Reemplaza "array" por tu arreglo "series", cada uno quedará en "item"
-    datos.forEach(item => {
-        // Escribe aquí el código para mostrar el nombre de la serie
-        console.log(item.serie);
+
+    datos.forEach(element => {
+        console.log(element.Serie)
+    });
+
+}
+
+//2
+longitud(series)
+
+function longitud(datos) {
+    console.log(datos.length)
+
+}
+
+//3
+Mayuscula(series)
+
+function Mayuscula(datos) {
+    datos.forEach(element => {
+        console.log(`El texto es: ${element.Serie.toUpperCase()}`)
     });
 }
 
-mostrarNombresSeries(series);
+//4
+Minus(series)
+
+function Minus(datos) {
+    datos.forEach(element => {
+        console.log(`El texto es: ${element.Serie.toLowerCase()}`)
+    });
+}
+
+//5
+
+function BuscarSerie(datos, nombre) {
+    datos.forEach(element => {
+        if (element.series == nombre) {
+            console.log(`Serie encontrada: ${element.Serie}`)
+        } else {
+            console.log(":(")
+        }
+    });
+
+
+}
